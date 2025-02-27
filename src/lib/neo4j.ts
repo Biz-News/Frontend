78d51558e7330d7321505b4e70f8c4179998c00e
Keemzino <69239293+jinho7@users.noreply.github.com>
2025-02-28 05:18:39 +0900

    Update neo4j.ts

--- src/lib/neo4j.ts
+++ src/lib/neo4j.ts
@@ -4,9 +4,9 @@ let driver: Driver;
 
 // Neo4j 드라이버 초기화
 export function initDriver() {
-  const uri = process.env.NEO4J_URI || 'bolt://localhost:7687';
+  const uri = process.env.NEO4J_URI || 'bolt://neo4j:7687';
   const user = process.env.NEO4J_USER || 'neo4j';
-  const password = process.env.NEO4J_PASSWORD || 'password';
+  const password = process.env.NEO4J_PASSWORD || '123456789';
   
   // 드라이버가 아직 초기화되지 않은 경우에만 초기화
   if (!driver) {
